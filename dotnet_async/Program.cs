b958f8d48b661571aebe5120d52970a12a642010
LuizHB23 <luizhenriquebarbosa21@gmail.com>
2025-03-11 21:17:40 -0300

    Atualizando repositório

--- dotnet_async/Program.cs
+++ dotnet_async/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿Task<string> conteudoTask = Task.Run(() => File.ReadAllText("voos.txt"));
+
+void LerArquivo()
+{
+    Task.Delay(new Random().Next(300,8000));
+    Console.WriteLine($"Conteúdo: \n{conteudoTask.Result}");
+}
+
+void ExibirRelatorio()
+{
+    Console.WriteLine("Executando relatório de compra de passagens!");
+    Task.Delay(new Random().Next(300,8000));
+}
+
+var task1 = Task.Run(() => LerArquivo());
+var task2 = Task.Run(() => ExibirRelatorio());
+
+Console.WriteLine("Outras operações.");
+Console.ReadKey();
